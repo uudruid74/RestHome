da2ef37d54122aa228e71f1d458761f27d8cd678
Evan Langlois <uudruid7@gmail.com>
2019-02-02 17:08:35 -0600

    Lots of bug fixes, performance enhancements.  Clickable icons!

--- ui/testing/custom.js
+++ ui/testing/custom.js
@@ -1,3 +1,6 @@
+DevCache = {};
+DevList = {};
+
 $.postJSON = function(url, func)
 {
     var sendInfo = {
@@ -31,7 +34,7 @@ function custom_set_subtitle() {
 function custom_get_dash() {
     $.postJSON("/getCustomDash", function(data) {
         if (data.hasOwnProperty('ok')) {
-        statusbar = '<div class="mdl-dash mdl-shadow--2dp">';
+        var statusbar = '<div class="mdl-dash mdl-shadow--2dp">';
         statusbar += '    <div class="center"><b>' + $('#subtitle').html() + '</b></div>';
         statusbar += '    <div class="center">' + data.ok + '</div>';
         statusbar += '</div>';
@@ -40,72 +43,132 @@ function custom_get_dash() {
         }
     });
 }
+
 function custom_get_devices() {
     $.postJSON("/listDevices", function(data) {
         $('#custom-content').html('');
         if (data.hasOwnProperty('ok')) {
-        Object.keys(data).forEach(function(key,index) {
-            if (key != 'ok' && key != 'default') {
-                ret = custom_build_device_card(key,data[key]);
-                $('#custom-content').append(ret);
-            }
-        })
+            DevList = {}
+            Object.keys(data).forEach(function(key,index) {
+                DevList[key] = data[key];
+                if (key != 'ok' && key != 'default') {
+                    ret = custom_build_device_card(key,data[key]);
+                    $('#custom-content').append(ret);
+                    custom_build_device_content(key,data[key],true);
+                }
+            })
         }
     })
 }
 
 function custom_build_device_card(dev,comment) {
+    var comment;
     if ( dev != comment ) {
         comment = dev + ": " + comment
     }
-    setTimeout (function() { custom_build_device_content(dev,comment); }, 500+Math.floor(Math.random() * 500));
-    startcard = '<div class="mdl-cell mdl-cell--4-col" id="'+dev+'-card"><div> </div>'
-    endcard = '</div>';
+    var startcard = '<div class="mdl-cell mdl-cell--4-col" id="'+dev+'-card"><div> </div>'
+    var endcard = '</div>';
     return startcard + endcard;
 }
-function custom_build_device_content(dev,comment) {
-    setTimeout (function() { custom_build_device_content(dev,comment); }, 5000+Math.floor(Math.random() * 5000));
 
-    extraclass = ''
-    $.postJSON("/"+dev+"/listStatus", function(data) {
-        statusinfo = '';
-        count = 0;
-        if (data.hasOwnProperty('ok')) {
-        Object.keys(data).forEach(function(key,index) {
-            if (key != 'ok' && key != 'default') {
-                count++;
-                lastvalue = data[key];
-                if (key == 'power' || key.toLowerCase() == dev.toLowerCase()) {
-                    if (data[key] == '1') {
-                        $('#'+dev+'img').addClass('poweron');
-                    }
-                    else if (data[key] == '0') {
-                        $('#'+dev+'img').addClass('poweroff');
-                    }
-                }
-                statusinfo += '<tr><td><b>' + key + '</b></td><td>' + data[key] + "</td></tr>\n";
-            }
+function clickimage(dev,clickkey,oldstatus,comment) {
+    console.log("Clicked on " + dev + " clickkey = " + clickkey);
+    var url;
+    if (clickkey == '') {
+        return;
+    }
+    if (clickkey == 'power') {
+        if (oldstatus == '1') {
+            url = "/"+dev+"/sendCommand/poweroff";
+        } else {
+            url = "/"+dev+"/sendCommand/poweron";
+        }
+    } else if (clickkey == 'enabled') {
+        if (oldstatus == '1') {
+            url = "/"+dev+"/setStatus/enabled/0";
+        } else {
+            url = "/"+dev+"/setStatus/enabled/1";
+        }
+    } else {
+        if (oldstatus == '1') {
+            url = "/"+dev+"/sendCommand/"+clickkey+"off";
+        } else {
+            url = "/"+dev+"/sendCommand/"+clickkey+"on";
+        }
+    }
+    $.postJSON(url, function() {
+            custom_build_device_content(dev,comment,false);
         });
-        $('#' + dev).html(statusinfo);
-        if (count == 1) {
-            if (lastvalue == '0') {
-                $('#'+dev+'img').addClass('poweroff');
+
+}
+
+function custom_build_device_content(dev,comment,loop) {
+    var clickkey = '';
+    var clickstatus = '';
+    var statusinfo = '';
+    var changed = false;
+
+    if (!(dev in DevCache)) {
+        innercard = '<div class="mdl-card mdl-shadow--2dp">'
+            + '<div class="card-title"><b>'+comment+'</b></div>'
+            + '<table class="card-interior">'
+            + '<tr class="card-interior-row">'
+            + '<td><img id="'+dev+'img" class="icon" '
+            + 'src="/getIcon/'+dev+'.svg" /></td>'
+            + '<td><table class="small data" id="'+ dev
+            + '"> </table></td></tr><tr><td colspan="3">'
+            + '</td></tr></table></div>';
+        $('#'+dev+'-card').html(innercard);
+        changed = true;
+        DevCache[dev] = {}
+    }
+    $.postJSON("/"+dev+"/listStatus", function(data) {
+        var count = 0;
+        if (data.hasOwnProperty('ok')) {
+            Object.keys(data).forEach(function(key,index) {
+                if (key != 'ok' && key != 'default') {
+                    count++;
+                    if (DevCache[dev][key] != data[key]) {
+                        DevCache[dev][key] = data[key];
+                        changed = true;
+                    }
+                    lastvalue = data[key];
+                    lastkey = key;
+                    if (key == 'power' || key == 'enabled' || key.toLowerCase() == dev.toLowerCase()) {
+                        if (data[key] == '1') {
+                            $('#'+dev+'img').removeClass('poweroff').addClass('poweron');
+                        }
+                        else if (data[key] == '0') {
+                            $('#'+dev+'img').removeClass('poweron').addClass('poweroff');
+                        }
+                        clickkey = key;
+                        clickstatus = data[key];
+                    }
+                    statusinfo += '<tr><td><b>' + key + '</b></td><td>' + data[key] + "</td></tr>\n";
+                }
+            });
+            if (changed) {
+                $('#' + dev).html(statusinfo);
+                $("#"+dev+"img").on('click',function() {
+                    clickimage(dev,clickkey,clickstatus,comment);
+                });
             }
-            else if (lastvalue == '1') {
-                $('#'+dev+'img').addClass('poweron');
+            if (count == 1) {
+                clickkey = lastkey;
+                clickstatus = lastvalue
+                if (lastvalue == '0') {
+                    $('#'+dev+'img').removeClass('poweron').addClass('poweroff');
+                }
+                else if (lastvalue == '1') {
+                    $('#'+dev+'img').removeClass('poweroff').addClass('poweron');
+                }
             }
         }
+        if (loop) {
+            setTimeout (function() {
+                custom_build_device_content(dev,comment,true);
+            }, 8000+Math.floor(Math.random() * 4000));
         }
     })
-    innercard = '<div class="mdl-card mdl-shadow--2dp">'
-        + '<div class="card-title"><b>'+comment+'</b></div>'
-        + '<table class="card-interior">'
-        + '<tr class="card-interior-row">'
-        + '<td><img id="'+dev+'img" class="icon'
-        + extraclass + '" src="/getIcon/'+dev+'.svg" /></td>'
-        + '<td><table class="small data" id="'+ dev
-        + '"> </table></td></tr><tr><td colspan="3">'
-        + '</td></tr></table></div>'
-    $('#'+dev+'-card').html(innercard);
 }
 
